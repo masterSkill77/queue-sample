e3ac0ceaf898a30a558945f22b19afe2c4f0dd76
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2024-03-07 16:19:58 +0300

    feat : update model

--- app/Models/People.php
+++ app/Models/People.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class People extends Model
 {
     use HasFactory;
-    protected $fillable = ['firstname', 'age', 'gender', 'id', 'country'];
+    protected $table = 'peoples';
+    protected $fillable = ['firstname', 'age', 'gender', 'country'];
 }
