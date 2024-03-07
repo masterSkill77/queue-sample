da3423630d66b5239394853fab7bcc2baa53a0ad
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2024-03-07 16:19:22 +0300

    feat : add controller

--- app/Http/Controllers/UploadExcelController.php
+++ app/Http/Controllers/UploadExcelController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Imports\PeoplesImport;
+use App\Jobs\StorePeopleJob;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -16,7 +18,7 @@ class UploadExcelController extends Controller
     {
         return view("upload");
     }
-    public function import(Request $request)
+    public function import(Request $request): JsonResponse
     {
         // Ensure that the 'file' field is present, is a file and is either in xlsx or xls format
         $request->validate([
@@ -24,7 +26,9 @@ class UploadExcelController extends Controller
         ]);
 
         $file = $request->file('file');
-        $array = Excel::toArray(new PeoplesImport, $file);
-        dd($array);
+        $array = (Excel::toArray(new PeoplesImport, $file))[0];
+        array_shift($array);
+        StorePeopleJob::dispatch($array);
+        return response()->json(['status' => 'Document in process']);
     }
 }
