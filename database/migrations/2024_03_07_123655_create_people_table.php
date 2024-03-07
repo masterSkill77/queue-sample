15cb012a977da545cc308122b38b268637ad7913
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2024-03-07 15:53:47 +0300

    feat : create model and migration for people

---
+++ database/migrations/2024_03_07_123655_create_people_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('peoples', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->integer('age');
+            $table->string('country');
+            $table->tinyInteger('gender');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('people');
+    }
+};
